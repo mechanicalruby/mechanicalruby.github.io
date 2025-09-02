d65f7bc567677a8fe9d7cf42124cddce5d4ca640
mechanicalruby <robbierocket05@gmail.com>
2025-09-02 11:43:55 -0700

    Correct a date in the log

--- src/routes/log/+page.svelte
+++ src/routes/log/+page.svelte
@@ -3,7 +3,7 @@
 <h1>Site changelog</h1>
 <ul>
     <li>29 Apr 2025 - Initial deployment</li>
-    <li>29 Apr 2025 - Simplified style, switched to a purple/yellow color palette</li>
+    <li>28 Jun 2025 - Simplified style, switched to a purple/yellow color palette</li>
     <li>22 Aug 2025 - Major code, content, and style update. Added various pages and simplified the codebase further.</li>
     <li>2 Sep 2025 - Switched to semantic header elements on blog posts.</li>
 </ul>
